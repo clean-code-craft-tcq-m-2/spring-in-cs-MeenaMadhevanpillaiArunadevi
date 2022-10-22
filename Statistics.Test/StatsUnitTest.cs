e691993fdbe411d255cc938a98926cd96d066ea0
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-10-22 08:13:33 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -35,9 +35,9 @@ namespace Statistics.Test
             var ledAlert = new LEDAlert();
             object[] alerters = {emailAlert, ledAlert};
 
-            const double maxThreshold = 10.2;
+            const float maxThreshold = 10.2F;
             var statsAlerter = new StatsAlerter(maxThreshold, alerters);
-            statsAlerter.checkAndAlert(new List<double>{0.2, 11.9, 4.3, 8.5});
+            statsAlerter.checkAndAlert(new List<float>{0.2, 11.9, 4.3, 8.5});
 
             Assert.True(emailAlert.emailSent);
             Assert.True(ledAlert.ledGlows);
